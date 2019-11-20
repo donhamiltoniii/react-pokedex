5e18145e06e3428ebd1cb936e8552af4e5b2c2ef
Don Hamilton III <donhamiltoniii@gmail.com>
2019-11-20 11:41:28 -0500

    rename dir and project name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { getPokemonCollection } from './utils/http'
+import { getPokemonCollection } from './utils/pokemon'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
