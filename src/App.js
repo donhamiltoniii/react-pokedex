55c910e3187f179bd85416cd8c085ef5a9c84107
Don Hamilton III <donhamiltoniii@gmail.com>
2019-11-20 12:05:37 -0500

    add initial content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
 
+import './App.css'
+
+import { getPokemonCollection } from './utils/pokemon'
+
 function App () {
+  const [limit, setLimit] = React.useState('')
+  const [offset, setOffset] = React.useState('')
+  const [pokemon, setPokemon] = React.useState([])
+
+  React.useEffect(async () => {
+    setPokemon(await getPokemonCollection({ limit: 20 }))
+  }, [])
+
+  const handleClick = async () => {
+    setPokemon(await getPokemonCollection({ limit, offset }))
+  }
+
+  const updateLimit = event => {
+    setLimit(event.target.value)
+  }
+
+  const updateOffset = event => {
+    setOffset(event.target.value)
+  }
+
   return (
     <div className="App">
-      Pokedex
+      <header>
+        <h1>Pokedex</h1>
+      </header>
+      <main>
+        <section>
+          <label>How many PokeMon to show: <input name="limit" onChange={updateLimit} placeholder="Number of pokemon" type="text" value={limit} /></label>
+        </section>
+        <section>
+          <label>Which ID to start with: <input name="offset" onChange={updateOffset} placeholder="Starting index" type="text" value={offset} /></label>
+        </section>
+        <button onClick={handleClick}>Get New List</button>
+        <ul>
+          {pokemon.map(({ name, url }, index) => <li key={index}><span>{name}</span>: <span>{url}</span></li>)}
+        </ul>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { getPokemonCollection } from './utils/pokemon'
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-getPokemonCollection({ limit: 9, offset: 494 }).then(console.log)
